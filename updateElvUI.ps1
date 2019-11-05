328bbd128ca1bd7f2c2cf92467742e921ef42a10
Florian Mautendorfer <florian.mautendorfer@gmail.com>
2019-11-05 15:26:43 +0100

    Fixed fileformats and added error-checking

--- updateElvUI.ps1
+++ updateElvUI.ps1
@@ -6,8 +6,7 @@ if (-Not (Test-Path -Path '.\wowpath.cfg')) {
     exit
 }
 
-$WOWPath = Get-Content '.\wowpath.cfg' | Out-String | ConvertFrom-StringData
-
+$WOWPath = Get-Content '.\wowpath.cfg' -Raw
 #Exit if the Path to WOW doesnt exist
 if (-Not (Test-Path -Path $WOWPath)){
     msg console /server:localhost "Ding Dong your wowpath is wrong!"
@@ -16,43 +15,39 @@ if (-Not (Test-Path -Path $WOWPath)){
 
 #If ElvUI is installed check the toc file for the installed version number
 if (Test-Path -Path $WOWPath/ElvUI/ElvUI.toc){
-    $versionOLD = Select-String -Path $WOWPath+'\ElvUI\ElvUI.toc' -Pattern 'Version\: ([0-9]+.[0-9]+)' -AllMatches | %% { $_.Matches.Groups[1] } | %% { $_.Value }
-    Remove-Item -Path '.\tmpfile'
+    $versionOLD = Select-String -Path $WOWPath'\ElvUI\ElvUI.toc' -Pattern 'Version\: ([0-9]+.[0-9]+)' -AllMatches | % { $_.Matches.Groups[1] } | % { $_.Value }
 }
 
-Invoke-WebRequest Invoke-WebRequest "https://www.tukui.org/download.php?ui=elvui#version" -OutFile elvui.html
-if($LASTEXITCODE -eq 1){
+Invoke-WebRequest "https://www.tukui.org/download.php?ui=elvui#version" -OutFile elvui.html
+if($error){
 	msg console /server:localhost "Error appeared, try again"
     Remove-Item -Path '.\elvui.html'
     exit
 }
 
-Select-String -Path elvui.html -Pattern '<b class=""Premium"""">([0-9]+.[0-9]+)</b>' -AllMatches | %% { $_.Matches.Groups[1] } | %% { $_.Value } | Out-File tmpfile -Encoding ascii -NoNewline
-if($LASTEXITCODE -eq 1){
+$version = Select-String -Path '.\elvui.html' -Pattern '<b class="Premium">([0-9]+.[0-9]+)</b>' -AllMatches | % { $_.Matches.Groups[1] } | % { $_.Value }
+if($error){
 	msg console /server:localhost "Error appeared, try again"
 	Remove-Item -Path '.\tmpfile'
 	exit
 }
 
-$version = Get-Content '.\tmpfile' | Out-String | ConvertFrom-StringData
-Remove-Item -Path '.\tmpfile'
-
 if($versionOLD -lt $version){
-    if (-Not (Test-Path -Path "elvui-"+$version+".zip")){
-        Invoke-WebRequest "https://www.tukui.org/downloads/elvui-"+$version"+.zip" -OutFile "elvui-"+$version+".zip"
-        if($LASTEXITCODE -eq 1){
+    if (-Not (Test-Path -Path "elvui-$version.zip")){
+        Invoke-WebRequest "https://www.tukui.org/downloads/elvui-$version.zip" -OutFile "elvui-$version.zip"
+        if($error){
             msg console /server:localhost "Error appeared, try again"
             Remove-Item -Path 'elvui-'+$version+'.zip'
             exit
         }
     }
-    Expand-Archive -Path '%CD%\elvui-%version%.zip' -DestinationPath '%wowPath%' -Force
-    if($LASTEXITCODE -eq 1){
+    Expand-Archive -Path ".\elvui-$version.zip" -DestinationPath $WOWPath -Force
+    if($error){
         msg console /server:localhost "Error appeared, try again"
-        Remove-Item -Path 'elvui-'+$version+'.zip'
+        Remove-Item -Path "elvui-$version.zip"
         exit
     }
-    msg console /server:localhost "Installed ElvUI version "+$version+" succesfully"
+    msg console /server:localhost "Installed ElvUI version "$version" succesfully"
     exit
 } else{
     msg console /server:localhost "Newest ElvUI version is already downloaded and installed BLYAT!"
